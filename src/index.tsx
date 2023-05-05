3faf07b6f1b58d107361bead7e596ac8867f3db9
Giudicelli <giudicellielias@gmail.com>
2023-05-05 09:10:26 -0300

    Correção da responsividade

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import Home from 'components/Home';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "index.css";
-
+import "style/normalize.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
